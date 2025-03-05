ade19da9d62792602bacdfb2a10f0c0f3f71948d
Alexandre Ayoub <ayoub.alexandre@gmail.com>
2025-03-05 17:28:46 +0100

    testing new api endpoint

--- script.js
+++ script.js
@@ -1,10 +1,6 @@
 let taxiFareApiUrl = 'https://y-1098474597248.europe-west1.run.app/predict';
 const centralCoordinates = [-74.00597, 40.71427]; // starting position [lng, lat]
 
-if (window.location.href.includes('https://taxifare.lewagon.com')) {
-  taxiFareApiUrl = 'https://y-1098474597248.europe-west1.run.app/predict';
-}
-
 mapboxgl.accessToken = 'pk.eyJ1Ijoia3Jva3JvYiIsImEiOiJja2YzcmcyNDkwNXVpMnRtZGwxb2MzNWtvIn0.69leM_6Roh26Ju7Lqb2pwQ';
 
 const displayMap = (start, stop) => {
